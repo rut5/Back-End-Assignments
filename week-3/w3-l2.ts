91dd4ba07f56b5d9187be1290a3af9c7a43a53fd
Rut <rutwintzell@outlook.com>
2026-02-03 13:41:58 +0100

    Update w3-l2.ts

--- week-3/w3-l2.ts
+++ week-3/w3-l2.ts
@@ -1,24 +1,32 @@
-const myPromise = new Promise ((resolve, reject) => {
-
-    let success = true;
-
-    if (success) {
-        resolve("Operation was successful");
-    } else {
-        reject("Operation failed");
-    }
-});
-
-myPromise
-.then((message) => {
-    console.log(message);
-})
-.catch((error) => {
-    console.log("Error: " + error);
-});
-
-const fetchAdviceByID = () => {
-
+interface AdviceSlip {
+  slip: {
+    id: number;
+    advice: string;
+  };
 }
 
-fetchAdviceByID(10);
+const coinFlip = new Promise<string>((resolve, reject) => {
+  let success = Math.random(); // generates a number between 0 and 1
+
+  if (success > 0.5) {
+    resolve("You win! Here is your advice: ");
+  } else {
+    reject("You lose! No advice for you today.");
+  }
+});
+
+coinFlip
+  .then((message) => {
+    console.log(message); // if successful it logs the win message
+    
+    return fetch("https://api.adviceslip.com/advice", { cache: "no-cache" });
+  })
+  .then((response) => {
+    return response.json(); // translates the data into JSON
+  })
+  .then((data: AdviceSlip) => {
+    console.log(data.slip.advice); // logs the advice
+  })
+  .catch((error) => {
+    console.log(error); // if unsuccessful it logs the error message
+  });
